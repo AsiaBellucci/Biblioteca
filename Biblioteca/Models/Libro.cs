c970b3f5e27c9967462ecf0f27a43122a1421f26
a.bellucci <a.bellucci@VETRYA-PC003>
2020-01-28 12:57:07 +0100

    Creati metodi della classe Libro

--- Biblioteca/Models/Libro.cs
+++ Biblioteca/Models/Libro.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace Biblioteca.Models
 {
+    /// <summary>
+    /// Rappresenta un libro e gestisce l'assegnazione degli stessi
+    /// </summary>
     public class Libro
     {
         private int id;
         private string titolo;
-        private Utente utente;
+        private Utente utente;// L'utente che ha in prestito il libro, se null il libro è disponibile
 
         public Libro(int id, string titolo)
         {
@@ -17,23 +20,42 @@ namespace Biblioteca.Models
             this.utente = null;
         }
 
-        public void assegnaLibro(Utente utente, int id)
+        #region metodi
+        /// <summary>
+        /// permette di assegnare un libro e ne verifica la dispponibilità
+        /// </summary>
+        /// <param name="richiedente">accetta in ingresso un oggetto di tipo utente</param>
+        public void assegnaLibro(Utente richiedente)
         {
 
             if (this.utente == null)
             {
-                this.utente = utente;
-                Console.WriteLine($"Il libro{id} è stato assegnato a {utente}");
+                this.utente = richiedente;
+                Console.WriteLine($"Il libro è stato assegnato a {richiedente.Nome} {richiedente.Cognome}");
+            }
+            else
+            {
+                Console.WriteLine("Il libro è già stato assegnato");
             }
         }
 
-        public void RestituireLibro(Utente utente)
+        /// <summary>
+        /// permette di restituire un libro e verifica se l'utente ne è in possesso
+        /// </summary>
+        /// <param name="restituente"></param>
+        public void restituzioneLibro(Utente restituente)
         {
-            if(utente)
+            if(this.utente != null)
             {
-                this.utente = null;
-                Console.WriteLine($"L'utente {utente} è stato rimosso");
+                Console.WriteLine($"L'utente {restituente.Nome} {restituente.Cognome} sta restituendo il libro");
+                restituente = null;
+            }
+            else
+            {
+                Console.WriteLine("Il libro è disponibile");
             }
         }
+#endregion
+
     }
 }
--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -15,19 +15,32 @@ namespace Biblioteca
 
         public static void addNewBook()
         {
-            Console.WriteLine("Inserisci id: ");
-            int Id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Inserisci titolo: ");
-            string Titolo = Console.ReadLine();
-            
-            Console.WriteLine("Inserisci nome utente: ");
-            string utente = Console.ReadLine();
-            
-            
+            //Console.WriteLine("Inserisci id: ");
+            //int Id = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Inserisci titolo: ");
+            //string Titolo = Console.ReadLine();
 
-            Libro l = new Libro(Id, Titolo);
-            List<Libro> Libri = new List<Libro>();
-            l.assegnaLibro(utente, Id);
+            //Console.WriteLine("Inserisci nome utente: ");
+            //Utente utente = Console.ReadLine();
+
+
+
+            //Libro l = new Libro(Id, Titolo);
+            //List<Libro> Libri = new List<Libro>();
+            //l.assegnaLibro(utente, Id);
+
+            //Console.WriteLine("Inserisci nome utente: ");
+            //string nome = Console.ReadLine();
+            //Console.WriteLine("Inserisci cognome utente: ");
+            //string cognome = Console.ReadLine();
+            Utente ut = new Utente("Luca","Verdi");
+            Utente ut2 = new Utente("Mario", "Rossi");
+            Libro l1 = new Libro(1, "tit1");
+            Libro l2 = new Libro(2, "tit2");
+
+            l1.assegnaLibro(ut);
+            l2.assegnaLibro(ut);
+            l1.restituzioneLibro(ut);
         }
     }
 }
